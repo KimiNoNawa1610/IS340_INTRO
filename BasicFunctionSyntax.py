28501e5f16b91a930df4162abca018b3c7bc8bdb
Nhan Vo <48186215+KimiNoNawa1610@users.noreply.github.com>
2022-07-28 21:08:26 -0700

    Create BasicFunctionSyntax.py

---
+++ BasicFunctionSyntax.py
@@ -0,0 +1,8 @@
+
+def main(): #function header
+    x=14 #local variable (only exist inside the function)
+    if 0<=x<100:
+        return True #evaluated to true
+    else:
+        return False #evaluated to false
+main() #calling the function
